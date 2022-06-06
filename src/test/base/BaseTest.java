ade2045bc2f8bb8175489e6738cce4a9c8b23c02
Ricardo Arnez <ricardoarnez@hotmail.com>
2022-06-06 19:02:00 -0400

    validation

--- src/test/base/BaseTest.java
+++ src/test/base/BaseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
--- src/test/tests/ShoppingCartTest.java
+++ src/test/tests/ShoppingCartTest.java
@@ -1,15 +1,11 @@
 package tests;
 
 import base.BaseTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
-
-import java.util.List;
 
 public class ShoppingCartTest extends BaseTest {
     HomePage homePage;
