146e471afd34646259e394a2829c665feafd2352
NikolayGarmaev <nikolaygarmaev91@gmail.com>
2022-06-07 19:27:54 +0300

    configuration setup

--- src/test/tests/AccountPageTest.java
+++ src/test/tests/AccountPageTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BasePage;
 import base.BaseTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -18,23 +19,39 @@ public class AccountPageTest extends BaseTest {
     AccountPage accountPage;
 
     @BeforeMethod
-    public void localSetUp(){
+    public void localSetUp() {
         homePage = new HomePage(getDriver());
     }
+
     @Test(testName = "us-8", description = "locating buttons on my personal page")
     public void test01() throws InterruptedException {
-        homePage.click(homePage.signinBtn);
 
-        basePage.sleep(2000L);
-        homePage.waitForElementVisibility(loginPage.emailField);
+        getDriver().findElement(By.className("login")).click();
+        getDriver().findElement(By.className("login")).click();
+        getDriver().findElement(By.name("email")).sendKeys("test1111@test1111.com");
+        getDriver().findElement(By.name("passwd")).sendKeys("qwerty123");
+        getDriver().findElement(By.id("SubmitLogin")).click();
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='Order history and details']")).isEnabled());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My credit slips']")).isEnabled());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My addresses']")).isEnabled());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My personal information']")).isEnabled());
+        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My wishlists']")).isEnabled());
 
-        loginPage.sendKeys(loginPage.emailField, "test1111@test1111.com");
-        loginPage.sendKeys(loginPage.passwordField, "qwerty123");
-        loginPage.click(loginPage.signinBtn);
+//        homePage.click(homePage.signinBtn);
+//        basePage.sleep(2000L);
+//        basePage.waitForElementClickability(loginPage.emailField);
+//        loginPage.emailField.sendKeys("test1111@test1111.com");
+//        loginPage.passwordField.sendKeys("qwerty123");
+//        loginPage.submitBtn.click();
 
-        WebElement[] btns = {accountPage.orderHistoryAndDetails, accountPage.myCreditSlips, accountPage.myAddresses, accountPage.myPersonalInformation, accountPage.myWishList};
-        for (WebElement btn : btns) {
-            Assert.assertTrue(btn.isEnabled());
-        }
+//        Assert.assertTrue(accountPage.orderHistoryAndDetails.isEnabled());
+
+
+//        WebElement[] btns = {accountPage.orderHistoryAndDetails, accountPage.myCreditSlips, accountPage.myAddresses, accountPage.myPersonalInformation, accountPage.myWishList};
+//        for (WebElement btn : btns) {
+//            Assert.assertTrue(btn.isEnabled());
+//        }
+//    }
+//}
     }
 }
