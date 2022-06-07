0fbf16b8b801c37a4c97a91b36346ac88934306f
NikolayGarmaev <nikolaygarmaev91@gmail.com>
2022-06-07 19:37:08 +0300

    test us 8

--- src/test/pages/LoginPage.java
+++ src/test/pages/LoginPage.java
@@ -5,21 +5,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utils.ConfigReader;
 
 public class LoginPage extends BasePage {
     protected WebDriver driver;
+
 
     public LoginPage(WebDriver driver){
         super(driver);
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    @FindBy(name = "email")
+    @FindBy(xpath = "//input[@name='email']")
     public WebElement emailField;
 
-    @FindBy(name = "passwd")
+    @FindBy(xpath = "//input[@name='passwd']")
     public WebElement passwordField;
 
     @FindBy(xpath = "//button[@id='SubmitLogin']")
     public WebElement submitBtn;
+    public void signIn(){
+        String filePath = "src/test/data/config/configuration.properties";
+
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.sendKeys(emailField, ConfigReader.readProperty(filePath, "username"));
+
+        loginPage.sendKeys(passwordField, ConfigReader.readProperty(filePath, "password"));
+        loginPage.click(submitBtn);
+    }
 }
--- src/test/tests/AccountPageTest.java
+++ src/test/tests/AccountPageTest.java
@@ -14,44 +14,29 @@ import pages.LoginPage;
 public class AccountPageTest extends BaseTest {
     HomePage homePage;
     LoginPage loginPage;
-    BasePage basePage;
-
     AccountPage accountPage;
 
     @BeforeMethod
-    public void localSetUp() {
+
+
+    public void localSetup() {
         homePage = new HomePage(getDriver());
+        loginPage = new LoginPage(getDriver());
+        accountPage = new AccountPage(getDriver());
     }
+
 
     @Test(testName = "us-8", description = "locating buttons on my personal page")
-    public void test01() throws InterruptedException {
-
-        getDriver().findElement(By.className("login")).click();
-        getDriver().findElement(By.className("login")).click();
-        getDriver().findElement(By.name("email")).sendKeys("test1111@test1111.com");
-        getDriver().findElement(By.name("passwd")).sendKeys("qwerty123");
-        getDriver().findElement(By.id("SubmitLogin")).click();
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='Order history and details']")).isEnabled());
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My credit slips']")).isEnabled());
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My addresses']")).isEnabled());
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My personal information']")).isEnabled());
-        Assert.assertTrue(getDriver().findElement(By.xpath("//span[text()='My wishlists']")).isEnabled());
-
-//        homePage.click(homePage.signinBtn);
-//        basePage.sleep(2000L);
-//        basePage.waitForElementClickability(loginPage.emailField);
-//        loginPage.emailField.sendKeys("test1111@test1111.com");
-//        loginPage.passwordField.sendKeys("qwerty123");
-//        loginPage.submitBtn.click();
-
-//        Assert.assertTrue(accountPage.orderHistoryAndDetails.isEnabled());
-
-
-//        WebElement[] btns = {accountPage.orderHistoryAndDetails, accountPage.myCreditSlips, accountPage.myAddresses, accountPage.myPersonalInformation, accountPage.myWishList};
-//        for (WebElement btn : btns) {
-//            Assert.assertTrue(btn.isEnabled());
-//        }
-//    }
-//}
+    public void test01() {
+        homePage.click(homePage.signinBtn);
+        loginPage.sendKeys(loginPage.emailField, "test1111@test1111.com");
+        loginPage.sendKeys(loginPage.passwordField, "qwerty123");
+        loginPage.click(loginPage.submitBtn);
+        WebElement[] btns = {accountPage.orderHistoryAndDetails, accountPage.myCreditSlips, accountPage.myAddresses, accountPage.myPersonalInformation, accountPage.myWishList};
+        for (WebElement btn : btns) {
+            Assert.assertTrue(btn.isEnabled());
+        }
     }
 }
+
+
